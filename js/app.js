851aad8b7980220e405063a2bbf005538b627dda
Mateus Riebeck <mateusavs@hotmail.com>
2024-09-07 13:02:45 -0300

    desafios

--- js/app.js
+++ js/app.js
@@ -59,3 +59,54 @@ function sortingList(list_) { // baseado no algorítimo de Fisher-Yates
         [list_[i-1], list_[iRandom]] = [list_[iRandom], list_[i-1]];
     }
 }
+
+// desafios
+
+function validacaoNumero(n) {
+    let z = parseFloat(n);
+
+    if (z < 0) {
+        console.log('Número negativo');
+    } else if (z === 0) {
+        console.log('Número é zero');
+    } else {
+        console.log('Número positivo');
+    }
+}
+
+function validarIdade(n) {
+    return n >=18 ? console.log('maior') : console.log('menor');
+}
+
+function stringVazia(string) {
+    return string === '' ? alert('string vazia') : alert('string não vazia');
+}
+
+function anoBissexto(ano) {
+    if ((ano % 4 === 0 && ano % 100 !==0) || (ano % 400 === 0)) {
+        return alert('Ano bissexto');
+    } else {
+        return alert('Ano não bissexto');
+    }
+}
+
+function media(...n) {
+        let zLength = n.length;
+        let z = n.reduce((total, valor) => total + valor, 0);
+        return alert(z / zLength);
+}
+
+function arrayLength(array) {
+    return alert(array.length);
+}
+
+function confirmarValorArray(x, array) {
+    if (array.includes(x)) {
+        return alert(`O valor ${x} está contido na lista`);
+    } else {
+        return alert(`O valor ${x} não está contido na lista`);
+
+    }
+}
+
+let arrayStrings = ['um', 'dois', 'tres'];
